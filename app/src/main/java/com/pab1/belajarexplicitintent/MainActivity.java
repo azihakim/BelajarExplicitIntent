a553bfe48e5b1358472e13afd61708c1bdae4e94
azihakim <azi.hakim@mhs.mdp.ac.id>
2022-10-10 15:05:07 +0700

    intent

--- app/src/main/java/com/pab1/belajarexplicitintent/MainActivity.java
+++ app/src/main/java/com/pab1/belajarexplicitintent/MainActivity.java
@@ -2,13 +2,64 @@ package com.pab1.belajarexplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Layout A");
+        etNama = findViewById(R.id.et_nama);
+
+        btnPindah = findViewById(R.id.btn_pindah);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+
+                if (nama.trim().equals("")){
+                    etNama.setError("Nama Harus Diisi");
+                }
+                else {
+                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    pindah.putExtra("xNama", nama);
+                    startActivity(pindah);
+                }
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
